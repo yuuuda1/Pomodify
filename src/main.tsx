a69d0d25215643fc56b76430948454ffed75d72e
yuuuda1 <yudai.kato@sun-asterisk.com>
2024-12-04 02:26:16 +0900

    fix: VercelのSpeed Insightsを削除

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Timer } from './components/Timer';
-import { SpeedInsights } from "@vercel/speed-insights/next"
-
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <SpeedInsights />
     <Timer />
   </StrictMode>,
 )
