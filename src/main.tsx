05b77c5b23946e8a9227bde37c856590efdf4dc5
yuuuda1 <yudai.kato@sun-asterisk.com>
2024-12-04 02:27:30 +0900

    fix: VercelのSpeed Insightsを追加

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Timer } from './components/Timer';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <SpeedInsights />
     <Timer />
   </StrictMode>,
 )
