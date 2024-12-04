b1a19a10d3ebeb7012ba14cc502f2480c4035885
yuuuda1 <yudai.kato@sun-asterisk.com>
2024-12-04 23:03:02 +0900

    fix: VercelのAnalyticsパッケージの追加

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Timer } from './components/Timer';
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <SpeedInsights />
+    <Analytics />
     <Timer />
   </StrictMode>,
 )
